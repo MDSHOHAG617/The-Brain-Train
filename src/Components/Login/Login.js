980042042c3b39d5046cef1a47a731e7235d1145
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-17 03:43:41 +0600

    Email Pass Login done!

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import SocialLogin from "./SocialLogin/SocialLogin";
-
+import app from "../../firebase.init";
+// import getAuth from "firebase/auth";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const auth = getAuth(app);
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    return (
+      <div>
+        <p>Registered User: {user.email}</p>
+      </div>
+    );
+  }
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleLoginBtn = () => {
+    createUserWithEmailAndPassword(email, password);
+  };
   return (
     <div className="">
       <div className="bg-slate-100 w-96 rounded mx-auto">
@@ -9,9 +44,11 @@ const Login = () => {
         <div className="mb-5">
           <label className="p-4 font-semibold">Email</label>
           <input
+            onChange={handleEmail}
             required
+            value={email}
             className="bg-sky-200 p-2 w-64 rounded-full"
-            type="Email"
+            type="email"
             placeholder="your email"
           />
         </div>
@@ -19,13 +56,18 @@ const Login = () => {
         <div className="">
           <label className="p-2 font-semibold ">Password</label>
           <input
+            onChange={handlePassword}
             required
+            value={password}
             className="bg-sky-200 p-2 w-64 rounded-full mr-2"
-            type="Password"
+            type="password"
             placeholder="password"
           />
         </div>
-        <button className="mt-5 p-2 bg-sky-200 hover:bg-red-200 rounded-full m-10">
+        <button
+          onClick={handleLoginBtn}
+          className="mt-5 p-2 bg-sky-200 hover:bg-red-200 rounded-full m-10"
+        >
           Login
         </button>
       </div>
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtb2NTleyc_7vwvPqHxmJKzsNi8QexwA4",
+  authDomain: "assignment-10-firebase-auth.firebaseapp.com",
+  projectId: "assignment-10-firebase-auth",
+  storageBucket: "assignment-10-firebase-auth.appspot.com",
+  messagingSenderId: "57665035348",
+  appId: "1:57665035348:web:da722fb931460de89ca1c1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
