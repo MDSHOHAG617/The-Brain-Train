099bbe221851f807c58fd9eb18dd2d14573348c6
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-17 22:54:47 +0600

    Footer Added!

---
+++ src/Components/Home/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-sky-200 p-8 text-xl font-mono">
+      <p>Copyright @ 2022 The Brain Train</p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import banner from "../images/banner.png";
+import Footer from "./Footer";
 import Services from "./Services";
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
       <img className="mx-auto my-auto w-8/12" src={banner} alt="" />
 
       <Services></Services>
+      <Footer></Footer>
     </div>
   );
 };
