7a866f3295829c501d3565304070e59370626dbc
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-18 20:42:21 +0600

    updating redmefile!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# Site Name: The Brain Train
 
+## live site link: [https://assignment-10-firebase-auth.web.app/]
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from "react";
 const NotFound = () => {
   return (
     <div>
-      <h1>Ops! 404 page not found</h1>
+      <h1 className="text-5xl text-red-500 mt-32">Ops! 404 page not found</h1>
     </div>
   );
 };
